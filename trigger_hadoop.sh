2f5ba3839efa54e4ad2e486c1cf680563769c2fe
ajayx29 <78142053+ajayx29@users.noreply.github.com>
2025-02-26 03:01:04 -0500

    Update trigger_hadoop.sh

--- trigger_hadoop.sh
+++ trigger_hadoop.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 echo "Connection Successful"
+while true; do
+    echo "This is an infinite loop with no exit condition!"
+done
 hadoop jar /usr/lib/hadoop-mapreduce/hadoop-mapreduce-examples.jar wordcount gs://dataproc-staging-us-central1-40833754326-zlfuzvnm/input.txt gs://dataproc-staging-us-central1-40833754326-zlfuzvnm/output4
