b22a0e8485e97c3aa75c791909151f8c28987fad
ajayx29 <78142053+ajayx29@users.noreply.github.com>
2025-02-26 18:03:15 -0500

    Update run_hadoop_job.py

--- run_hadoop_job.py
+++ run_hadoop_job.py
@@ -7,7 +7,7 @@ def run_hadoop_job():
         hadoop_command = [
             "hadoop", "jar", "/usr/lib/hadoop-mapreduce/hadoop-mapreduce-examples.jar",
             "wordcount", "gs://dataproc-staging-us-central1-40833754326-zlfuzvnm/input.txt",
-            "gs://dataproc-staging-us-central1-40833754326-zlfuzvnm/output"
+            "gs://dataproc-staging-us-central1-40833754326-zlfuzvnm/output1"
         ]
         subprocess.run(hadoop_command, check=True)
         print("Hadoop job triggered successfully!")
