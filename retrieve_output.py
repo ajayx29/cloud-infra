21c2ac308975c6ef7cad8ae351d4fb7996a1c633
ajayx29 <78142053+ajayx29@users.noreply.github.com>
2025-02-26 18:18:32 -0500

    Update retrieve_output.py

--- retrieve_output.py
+++ retrieve_output.py
@@ -2,14 +2,17 @@ import subprocess
 import sys
 
 def retrieve_hadoop_output():
+    """Retrieves output from the Hadoop job stored in GCS."""
     try:
         print("Retrieving Hadoop job output...")
-        gsutil_command = ["gsutil", "cat", "gs://dataproc-staging-us-central1-40833754326-zlfuzvnm/output1/*"]
+        gsutil_command = ["gsutil", "cat", "gs://dataproc-staging-us-central1-40833754326-zlfuzvnm/output1/**"]
         subprocess.run(gsutil_command, check=True)
         print("Hadoop job output retrieved successfully!")
+        return True
     except subprocess.CalledProcessError:
         print("Failed to retrieve Hadoop job output!", file=sys.stderr)
-        sys.exit(1)
+        return False
 
 if __name__ == "__main__":
-    retrieve_hadoop_output()
+    success = retrieve_hadoop_output()
+    sys.exit(0 if success else 1)
